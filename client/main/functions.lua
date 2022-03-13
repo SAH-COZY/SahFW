d9e036f95adbdf06f3b17ae51980273c7f42d677
SAHCOZY <sahcozyy@gmail.com>
2022-03-13 12:26:26 +0100

    KeyboardInput function added

--- client/main/functions.lua
+++ client/main/functions.lua
@@ -3,3 +3,24 @@ function _DisableControls()
         DisableControlAction(0, v, true)
     end
 end
+
+function KeyboardInput(entryTitle, textEntry, inputText, maxLength)
+	AddTextEntry(entryTitle, textEntry)
+	DisplayOnscreenKeyboard(1, entryTitle, '', inputText, '', '', '', maxLength)
+	blockinput = true
+
+	while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+		Citizen.Wait(0)
+	end
+
+	if UpdateOnscreenKeyboard() ~= 2 then
+		local result = GetOnscreenKeyboardResult()
+		Citizen.Wait(500)
+		blockinput = false
+		return result
+	else
+		Citizen.Wait(500)
+		blockinput = false
+		return nil
+	end
+end
