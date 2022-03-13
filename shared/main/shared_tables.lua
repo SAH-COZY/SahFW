b8601a19b8fd4f4d554ec64ca71579f897277c30
SAHCOZY <sahcozyy@gmail.com>
2022-03-13 13:12:04 +0100

    Added some keys to disable IG

--- shared/main/shared_tables.lua
+++ shared/main/shared_tables.lua
@@ -1,3 +1,3 @@
 SharedTables = {}
-SharedTables.ControlsToDisable = {0, --[[12, 13, 14, 15, 16, 17, 37,--]] 85, 99, 100, 158, 159, 160, 161, 162, 163, 164, 165, 261, 262, 332, 333}
+SharedTables.ControlsToDisable = {0, 12, 13, 14, 15, 16, 17, 37, 85, 99, 100, 158, 159, 160, 161, 162, 163, 164, 165, 261, 262, 332, 333}
 SharedTables.LockedParts = {}
